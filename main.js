e2ffde18a0d6515d6158648e9a245f52d1ab36ff
Jabborov-Sardor <jabborovsardor0097.net@gmail.com>
2024-11-13 09:39:00 +0500

    not modul bor ekan ochirib tashadim

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 "use strict";
 let elList = document.querySelector(".todo-list");
+function rendTodo(arr, list) {
+    list.innerHTML = ``;
+    arr.forEach((item, index) => {
+        let elItem = document.createElement('li');
+        elItem.className = "flex items-center justify-between p-2bg-slate-200 rounded-md ";
+        elItem.innerHTML = `
+        <div class="flex">
+        <img class = "img-render" src =${item.img}  />
+        <strong>${item.title}</strong>
+        <p>${item.info}</p>
+        </div>
+        `;
+        list.append(elItem);
+    });
+}
 function renderJSON(API) {
-    fetch(API).then((res) => res.json()).then(data => {
-        console.log(data.products);
+    fetch(API).then((res) => res.json()).then((data) => {
+        const products = data.products.map((item) => {
+            const obj = {
+                id: item.id,
+                title: item.title,
+                // img: item.img[0],
+                info: item.description
+            };
+            return obj;
+        });
+        console.log(products);
+        rendTodo(products, elList);
     });
 }
 renderJSON("https://dummyjson.com/products");
--- main.ts
+++ main.ts
@@ -1,12 +1,45 @@
 let elList: Element | null = document.querySelector(".todo-list")
 
 interface JSONType {
-
+    id:number,
+    title:string,
+    img: string,
+    info:string
 }
 
+function rendTodo(arr:JSONType[], list:Element | null) {
+    (list as HTMLUListElement).innerHTML=``
+    arr.forEach((item:JSONType, index:number ) => {
+        let elItem:Element | null = document.createElement('li')
+        elItem.className="flex items-center justify-between p-2bg-slate-200 rounded-md "
+        elItem.innerHTML = `
+        <div class="flex">
+        <img class = "img-render" src =${item.img}  />
+        <strong>${item.title}</strong>
+        <p>${item.info}</p>
+        </div>
+        `;
+        (list as HTMLUListElement ).append(elItem)
+
+   
+
+    } )
+} 
+
 function renderJSON(API:string):void{
-    fetch(API).then((res:any) => res.json()).then (data => {
-        console.log(data.products ); 
+    fetch(API).then((res:any) => res.json()).then ((data:any) => {
+        const products = data.products.map((item:any) =>{
+            const obj = {
+                id:item.id,
+                title:item.title,
+                // img: item.img[0],
+                info:item.description
+            }
+            return obj
+        })
+        console.log(products);
+        
+        rendTodo(products, elList) 
     })
     
     
